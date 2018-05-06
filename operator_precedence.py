cf60704bad4f6af70eb681fdc6b1ebae72e2608c
Royal Bhati <royal240596@gmail.com>
2018-05-06 19:02:40 +0530

    Update operator_precedence.py

--- operator_precedence.py
+++ operator_precedence.py
@@ -4,25 +4,25 @@ import csv
 import pprint
 import numpy as np
 class Stack:
-     def __init__(self):
-         self.items = []
+    def __init__(self):
+        self.items = []
 
-     def isEmpty(self):
-         return self.items == []
+    def isEmpty(self):
+        return self.items == []
 
-     def push(self, item):
-         self.items.append(item)
+    def push(self, item):
+        self.items.append(item)
 
-     def pop(self):
-         return self.items.pop()
+    def pop(self):
+        return self.items.pop()
 
-     def peek(self):
-         return self.items[len(self.items)-1]
+    def peek(self):
+        return self.items[len(self.items)-1]
 
-     def size(self):
-         return len(self.items)
-     def items(self):
-         return self.items    
+    def size(self):
+        return len(self.items)
+    def items(self):
+        return self.items    
 
 def get_prec(element,i):
 	for x in range(len(order_table[0])):
